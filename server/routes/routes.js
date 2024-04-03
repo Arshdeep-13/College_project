4f1f26d1211f220bad3b91ea95821d48cbca2f90
ghoshariji <144580430+ghoshariji@users.noreply.github.com>
2024-04-03 19:51:55 +0530

    Update routes.js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,10 +1,10 @@
 const Route = require("express").Router();
 const userController = require("../controllers/userController.js");
 const expController = require("../controllers/expController.js");
-
+const middleware = require("../middleware/authMiddleware.js")
 Route.get("/", userController.Func);
 Route.post("/signup", userController.signup);
 Route.post("/login", userController.login);
-Route.post("/experience", expController.expData);
+Route.post("/experience",middleware, expController.expData);
 
 module.exports = Route;
