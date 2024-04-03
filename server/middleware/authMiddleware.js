ef4afc9c3d4babd77ab73a8089d731fd863676c5
ghoshariji <144580430+ghoshariji@users.noreply.github.com>
2024-04-03 19:51:08 +0530

    Update authMiddleware.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,1 +1,16 @@
-hello
+const jwt = require("jsonwebtoken");
+module.exports = (req,res,next) =>{
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        const decodetoken = jwt.verify(token,process.env.SECRET_KEy);
+        const userId = decodetoken.userId;
+        req.body.userId = userId
+        next()
+    } catch (error) {
+        return res.status(409).send({
+            message:"You are not authorized",
+            success:false
+        })
+    }
+    
+}
